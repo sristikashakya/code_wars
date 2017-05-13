4b24269cbd6c5df29e1e9033634190a6c219f128
sristikashakya123@gmail.com <sristikashakya123@gmail.com>
2017-05-13 20:48:14 +0545

    code-wars

--- Short_long.rb
+++ Short_long.rb
@@ -2,5 +2,5 @@ def solution(a, b)
   array=Array.new
   array.push(a.to_s)
   array.push(b.to_s)
-  array[0] > array[1] ? array[1]+array[0]+array[1] : array[0]+array[1]+array[0]
+  Integer(array[0]) > Integer(array[1])? array[1]+array[0]+array[1] : array[0]+array[1]+array[0]
 end
