e82f2d1611fe79f563b3860d9482b390d8ef7620
sristikashakya123@gmail.com <sristikashakya123@gmail.com>
2017-05-15 13:54:33 +0545

    code-wars

--- Short_long.rb
+++ Short_long.rb
@@ -1,6 +1,6 @@
 def solution(a, b)
   array=Array.new
-  array.push(a.to_s)
-  array.push(b.to_s)
-  Integer(array[0]) > Integer(array[1])? array[1]+array[0]+array[1] : array[0]+array[1]+array[0]
+  array.push(a)
+  array.push(b)
+  array[0].length > array[1].length ? array[1]+array[0]+array[1] : array[0]+array[1]+array[0]
 end
